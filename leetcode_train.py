3f6c5f5870a34e5125dc719efe8c70c9fa52ede2
NewaysNju <NewaysNju@gmail.com>
2018-04-10 00:32:23 +0800

    add problem 1

--- leetcode_train.py
+++ leetcode_train.py
@@ -87,5 +87,19 @@ class Solution:
         return M
 
 
+# 1. two sum
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
 if __name__ == '__main__':
     pass
